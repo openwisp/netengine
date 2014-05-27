9fc7a1b0074f4db62027a883b4cca097a4993ff1
Federico Capoano <nemesis@ninux.org>
2014-05-27 11:41:58 +0200

    snmp.openwrt optimization (result caching)

--- netengine/backends/snmp/openwrt.py
+++ netengine/backends/snmp/openwrt.py
@@ -42,6 +42,7 @@ class OpenWRT(SNMP):
 
     @property
     def manufacturer(self):
+        # TODO: this is dangerous, it might not work in all cases
         return self.get_manufacturer(self.interfaces_MAC[1]['mac_address'])
 
     @property
@@ -74,56 +75,77 @@ class OpenWRT(SNMP):
             value_to_retr.append(int(tmp[i][0][1]))
         return value_to_retr
 
+    _interfaces = None
+
     def get_interfaces(self):
         """
         returns the list of all the interfaces of the device
         """
-        interfaces = []
-        value_to_get = '1.3.6.1.2.1.2.2.1.2.'
-        for i in self._value_to_retrieve():
-            value_to_get1 = value_to_get+str(i)
-            if value_to_get1:
-                interfaces.append(self.get_value(value_to_get1))
-        return filter(None,interfaces)
+        if self._interfaces is None:
+            interfaces = []
+            value_to_get = '1.3.6.1.2.1.2.2.1.2.'
+
+            for i in self._value_to_retrieve():
+                value_to_get1 = value_to_get+str(i)
+
+                if value_to_get1:
+                    interfaces.append(self.get_value(value_to_get1))
+
+            self._interfaces = filter(None, interfaces)
+
+        return self._interfaces
+
+    _interfaces_MAC = None
 
     @property
     def interfaces_MAC(self):
         """
         Returns an ordered dict with the hardware address of every interface
         """
-        results = []
-        mac1 = []
-        mac = self.next('1.3.6.1.2.1.2.2.1.6.')[3]
-        for i in range(1, len(mac) + 1):
-            mac1.append(self.get_value('1.3.6.1.2.1.2.2.1.6.' + str(i)))
-        mac_trans = []
-        for i in mac1:
-            mac_trans.append(':'.join(binascii.b2a_hex(i)[a:a+2] for a in range(0, 12, 2) if i != ""))
-        for i in range(0, len(self.get_interfaces())):
-            result = self._dict({
-                "name" : self.get_interfaces()[i],
-                "mac_address" : mac_trans[i]
-            })
-            results.append(result)
-        return results
+        if self._interfaces_MAC is None:
+            results = []
+            mac1 = []
+            mac = self.next('1.3.6.1.2.1.2.2.1.6.')[3]
+            for i in range(1, len(mac) + 1):
+                mac1.append(self.get_value('1.3.6.1.2.1.2.2.1.6.' + str(i)))
+            mac_trans = []
+            for i in mac1:
+                mac_trans.append(':'.join(binascii.b2a_hex(i)[a:a+2] for a in range(0, 12, 2) if i != ""))
+            for i in range(0, len(self.get_interfaces())):
+                result = self._dict({
+                    "name" : self.get_interfaces()[i],
+                    "mac_address" : mac_trans[i]
+                })
+                results.append(result)
+
+            self._interfaces_MAC = results
+
+        return self._interfaces_MAC
+
+    _interfaces_mtu = None
 
     @property
     def interfaces_mtu(self):
         """
         Returns an ordereed dict with the interface and its MTU
         """
-        results = []
-        starting = "1.3.6.1.2.1.2.2.1.2."
-        tmp = list(starting)
-        tmp[18] = str(4)
-        to = ''.join(tmp)
-        for i in self._value_to_retrieve():
-            result = self._dict({
-                "name" : self.get_value(starting + str(i)),
-                "mtu" : int(self.get_value(to + str(i)))
-            })
-            results.append(result)
-        return results
+        if self._interfaces_mtu is None:
+            results = []
+            starting = "1.3.6.1.2.1.2.2.1.2."
+            tmp = list(starting)
+            tmp[18] = str(4)
+            to = ''.join(tmp)
+
+            for i in self._value_to_retrieve():
+                result = self._dict({
+                    "name" : self.get_value(starting + str(i)),
+                    "mtu" : int(self.get_value(to + str(i)))
+                })
+                results.append(result)
+
+            self._interfaces_mtu = results
+
+        return self._interfaces_mtu
 
     _interfaces_speed = None
 
@@ -177,72 +199,126 @@ class OpenWRT(SNMP):
 
         return self._interfaces_speed
 
+    _interfaces_state = None
+
     @property
     def interfaces_state(self):
         """
         Returns an ordereed dict with the interfaces and their state (up, down)
         """
-        results = []
-        starting = "1.3.6.1.2.1.2.2.1.2."
-        operative = "1.3.6.1.2.1.2.2.1.8."
-        tmp = list(starting)
-        tmp[18] = str(4)
-        for i in self._value_to_retrieve():
-            if self.get_value(starting + str(i)) != "" :
-                if int(self.get_value(operative + str(i))) == 1:
+        if self._interfaces_state is None:
+            results = []
+            starting = "1.3.6.1.2.1.2.2.1.2."
+            operative = "1.3.6.1.2.1.2.2.1.8."
+            tmp = list(starting)
+            tmp[18] = str(4)
+            for i in self._value_to_retrieve():
+                if self.get_value(starting + str(i)) != "" :
+                    if int(self.get_value(operative + str(i))) == 1:
+                        result = self._dict({
+                            "name" : self.get_value(starting + str(i)),
+                            "state" : "up"
+                        })
+                        results.append(result)
+                    else:
+                        result = self._dict({
+                            "name" : self.get_value(starting + str(i)),
+                            "state" : "down"
+                        })
+                        results.append(result)
+                elif self.get_value(starting + str(i)) == "" :
                     result = self._dict({
-                        "name" : self.get_value(starting + str(i)),
-                        "state" : "up"
-                    })
+                            "name" : "",
+                            "state" : ""
+                        })
                     results.append(result)
-                else:
-                    result = self._dict({
-                        "name" : self.get_value(starting + str(i)),
-                        "state" : "down"
-                    })
-                    results.append(result)
-            elif self.get_value(starting + str(i)) == "" :
-                result = self._dict({
-                        "name" : "",
-                        "state" : ""
-                    })
-                results.append(result)
-        return results
+
+            self._interfaces_state = results
+
+        return self._interfaces_state
+
+    _interfaces_bytes = None
 
     @property
     def interfaces_bytes(self):
         """
         Returns an ordereed dict with the interface and its tx and rx octets (1 octet = 1 byte = 8 bits)
         """
-        results = []
-        starting = "1.3.6.1.2.1.2.2.1.2."
-        starting_rx = "1.3.6.1.2.1.2.2.1.10."
-        starting_tx = "1.3.6.1.2.1.2.2.1.16."
-        for i in self._value_to_retrieve():
-            result = self._dict({
-                "name" : self.get_value(starting + str(i)),
-                "tx" : int(self.get_value(starting_tx + str(i))),
-                "rx" : int(self.get_value(starting_rx + str(i))),
-            })
-            results.append(result)
-        return results
+        if self._interfaces_bytes is None:
+            results = []
+            starting = "1.3.6.1.2.1.2.2.1.2."
+            starting_rx = "1.3.6.1.2.1.2.2.1.10."
+            starting_tx = "1.3.6.1.2.1.2.2.1.16."
+
+            for i in self._value_to_retrieve():
+                result = self._dict({
+                    "name" : self.get_value(starting + str(i)),
+                    "tx" : int(self.get_value(starting_tx + str(i))),
+                    "rx" : int(self.get_value(starting_rx + str(i))),
+                })
+                results.append(result)
+
+            self._interfaces_bytes = results
+
+        return self._interfaces_bytes
+
+    _interfaces_type = None
 
     @property
     def interfaces_type(self):
         """
         Returns an ordered dict with the interface type (e.g Ethernet, loopback)
         """
-        types = {"6" : "ethernetCsmacd", "24" : "softwareLoopback", "131" : "tunnel"}
-        results = []
-        starting = "1.3.6.1.2.1.2.2.1.2."
-        types_oid = "1.3.6.1.2.1.2.2.1.3."
-        for i in self._value_to_retrieve():
-            result = self._dict({
-                "name" : self.get_value(starting + str(i)),
-                "type" : types[self.get_value(types_oid + str(i))],
-            })
-            results.append(result)
-        return results
+        if self._interfaces_type is None:
+            types = {"6" : "ethernetCsmacd", "24" : "softwareLoopback", "131" : "tunnel"}
+            results = []
+            starting = "1.3.6.1.2.1.2.2.1.2."
+            types_oid = "1.3.6.1.2.1.2.2.1.3."
+            for i in self._value_to_retrieve():
+                result = self._dict({
+                    "name" : self.get_value(starting + str(i)),
+                    "type" : types[self.get_value(types_oid + str(i))],
+                })
+                results.append(result)
+            self._interfaces_type = results
+
+        return self._interfaces_type
+
+    _interface_addr_and_mask = None
+
+    @property
+    def interface_addr_and_mask(self):
+        """
+        TODO: this method needs to be simplified and explained
+        """
+        if self._interface_addr_and_mask is None:
+            interface_name = self.get_interfaces()
+
+            for i in range(0, len(interface_name)):
+                self._interface_dict[self._value_to_retrieve()[i]] = interface_name[i]
+
+            interface_ip_address = self.next("1.3.6.1.2.1.4.20.1.1")[3]
+            interface_index = self.next("1.3.6.1.2.1.4.20.1.2")[3]
+            interface_netmask = self.next("1.3.6.1.2.1.4.20.1.3")[3]
+
+            results = {}
+
+            for i in range(0, len(interface_ip_address)):
+                a = interface_ip_address[i][0][1].asNumbers()
+                ip_address = '.'.join(str(a[i]) for i in range(0, len(a)))
+                b = interface_netmask[i][0][1].asNumbers()
+                netmask = '.'.join(str(b[i]) for i in range(0, len(b)))
+
+                name = self._interface_dict[int(interface_index[i][0][1])]
+
+                results[name] = {
+                    "address" : ip_address,
+                    "netmask" : netmask
+                }
+
+            self._interface_addr_and_mask = results
+
+        return self._interface_addr_and_mask
 
     @property
     def interfaces_to_dict(self):
@@ -305,42 +381,6 @@ class OpenWRT(SNMP):
             # })
             results.append(result)
         return results
-
-    _interface_addr_and_mask = None
-
-    @property
-    def interface_addr_and_mask(self):
-        """
-        TODO: this method needs to be simplified and explained
-        """
-        if self._interface_addr_and_mask is None:
-            interface_name = self.get_interfaces()
-
-            for i in range(0, len(interface_name)):
-                self._interface_dict[self._value_to_retrieve()[i]] = interface_name[i]
-
-            interface_ip_address = self.next("1.3.6.1.2.1.4.20.1.1")[3]
-            interface_index = self.next("1.3.6.1.2.1.4.20.1.2")[3]
-            interface_netmask = self.next("1.3.6.1.2.1.4.20.1.3")[3]
-
-            results = {}
-
-            for i in range(0, len(interface_ip_address)):
-                a = interface_ip_address[i][0][1].asNumbers()
-                ip_address = '.'.join(str(a[i]) for i in range(0, len(a)))
-                b = interface_netmask[i][0][1].asNumbers()
-                netmask = '.'.join(str(b[i]) for i in range(0, len(b)))
-
-                name = self._interface_dict[int(interface_index[i][0][1])]
-
-                results[name] = {
-                    "address" : ip_address,
-                    "netmask" : netmask
-                }
-
-            self._interface_addr_and_mask = results
-
-        return self._interface_addr_and_mask
 
     @property
     def RAM_total(self):
