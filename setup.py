936bf923d5ce8364477ec4afbb5a55c72d6eff40
Federico Capoano <nemesis@ninux.org>
2014-11-14 19:54:58 +0100

    Improved setup.py: get requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -3,6 +3,20 @@ from setuptools import setup, find_packages
 from netengine import get_version
 
 
+def get_install_requires():
+    """
+    parse requirements.txt, ignore links, exclude comments
+    """
+    requirements = []
+    for line in open('requirements.txt').readlines():
+        # skip to next iteration if comment or empty line
+        if line.startswith('#') or line == '' or line.startswith('http') or line.startswith('git'):
+            continue
+        # add line to requirements
+        requirements.append(line)
+    return requirements
+
+
 setup(
     name='netengine',
     version=get_version(),
@@ -13,10 +27,7 @@ setup(
     license='MIT',
     url='https://github.com/ninuxorg/netengine',
     packages=find_packages(exclude=['tests', 'tests.*', 'docs', 'docs.*']),
-    install_requires=[
-        'paramiko',
-        'pysnmp'
-    ],
+    install_requires=get_install_requires(),
     zip_safe=False,
     classifiers=[
         'Development Status :: 1 - Planning',
