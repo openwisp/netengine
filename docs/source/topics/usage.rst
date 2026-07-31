ac077ec87c01f9bf0a077637622d4f68bd9d9063
Federico Capoano <f.capoano@openwisp.io>
2026-07-31 13:08:42 -0300

    [qa] Modernize QA tooling

--- docs/source/topics/usage.rst
+++ docs/source/topics/usage.rst
@@ -1,61 +1,79 @@
-*****
 Usage
-*****
+=====
 
-The usage of Netengine module requires it to be installed properly as explained in :doc:`index<../index>`.
-If you have an installation under a virtualenv, enter the folder /bin and type::
+The usage of Netengine module requires it to be installed properly as
+explained in :doc:`index <../index>`. If you have an installation under a
+virtualenv, enter the folder /bin and type:
+
+::
 
     source activate
 
-otherwise (if you have installed globally) just open an editor as bpython and you we are ready to go.
+otherwise (if you have installed globally) just open an editor as bpython
+and you we are ready to go.
 
 These are the main steps to follow to use the module:
 
- * import the correct backend and supported framework
- * declare a device using the proper constructor
- * invoke methods over the device just declared
+    - import the correct backend and supported framework
+    - declare a device using the proper constructor
+    - invoke methods over the device just declared
 
-So we have::
+So we have:
 
- from netengine.backends.<backend_name> import <supported_firmware>
+::
 
- <device_name> = supported_firmware_constructor
+    from netengine.backends.<backend_name> import <supported_firmware>
 
-To invoke methods over the just declared device it's necessary to use the dot notation as::
+    <device_name> = supported_firmware_constructor
 
- <device_name>.<method or property>
+To invoke methods over the just declared device it's necessary to use the
+dot notation as:
 
+::
+
+    <device_name>.<method or property>
 
 Further example will be found inside dedicated docs for every backend
 
-*************
 Running tests
-*************
+=============
 
-Install test reqirements::
+Install test reqirements:
+
+::
 
     pip install -r reqirements.txt
     pip install -r requirements-test.txt
 
-Clone repo::
+Clone repo:
+
+::
 
     git clone git://github.com/openwisp/netengine
 
     ./runtests.py
 
-To run tests on real devices, first copy the settings file::
+To run tests on real devices, first copy the settings file:
+
+::
 
     cp test-settings.example.json test-settings.json
 
-Then change the credentials accordingly, now run tests with::
+Then change the credentials accordingly, now run tests with:
+
+::
 
     DISABLE_MOCKS=1 TEST_SETTINGS_FILE='test-settings.json' ./runtests.py
 
-See test coverage with::
+See test coverage with:
+
+::
 
     nose2 --with-coverage
 
-Run specific tests by specifying the relative path::
+Run specific tests by specifying the relative path:
+
+::
 
     # base tests
     nose2 tests.base
--- netengine/backends/base.py
+++ netengine/backends/base.py
@@ -3,29 +3,27 @@ from collections import OrderedDict
 
 from netaddr import EUI, NotRegisteredError
 
-__all__ = ['BaseBackend']
+__all__ = ["BaseBackend"]
 
 
 class BaseBackend(object):
-    """
-    Base NetEngine Backend
-    """
+    """Base NetEngine Backend"""
 
     __netengine__ = True
     _dict = OrderedDict
 
     def __str__(self):
-        raise NotImplementedError('Not implemented, must be extended')
+        raise NotImplementedError("Not implemented, must be extended")
 
     def __repr__(self):
         """returns unicode string represantation"""
         return self.__str__()
 
     def validate(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     def to_dict(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     def to_json(self, **kwargs):
         dictionary = self.to_dict()
@@ -33,105 +31,99 @@ class BaseBackend(object):
 
     @property
     def os(self):
-        """
-        Not Implemented
+        """Not Implemented
 
-        should return a tuple in which
-        the first element is the OS name and
-        the second element is the OS version
+        should return a tuple in which the first element is the OS name
+        and the second element is the OS version
         """
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def name(self):
-        """
-        Not Implemented
+        """Not Implemented
 
         should return a string containing the device name
         """
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def model(self):
-        """
-        Not Implemented
+        """Not Implemented
 
         should return a string containing the device model
         """
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def RAM_total(self):
-        """
-        Not Implemented
+        """Not Implemented
 
         should return a string containing the device RAM in bytes
         """
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def uptime(self):
-        """
-        Not Implemented
+        """Not Implemented
 
-        should return an integer representing the number of seconds of uptime
+        should return an integer representing the number of seconds of
+        uptime
         """
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def uptime_tuple(self):
-        """
-        Not Implemented
+        """Not Implemented
 
         should return tuple (days, hours, minutes)
         """
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def ethernet_standard(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def ethernet_duplex(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def wireless_channel_width(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def wireless_mode(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def wireless_channel(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def wireless_output_power(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def wireless_dbm(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     @property
     def wireless_noise(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     # TODO: this sucks
     @property
     def olsr(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     def get_interfaces(self):
-        raise NotImplementedError('Not implemented')
+        raise NotImplementedError("Not implemented")
 
     def get_manufacturer(self, mac_address):
         """returns the manufacturer of the network interface"""
         if not mac_address:
-            return ''
+            return ""
         try:
             return EUI(mac_address).oui.registration().org
         except NotRegisteredError:
-            return ''
+            return ""
